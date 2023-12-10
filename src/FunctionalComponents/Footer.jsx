186f3f6de6e496ba8dac66f4089ab999cd1cc548
itaadil <marc69199@gmail.com>
2023-12-10 13:19:18 +0530

    NewsPeek is an app that lets users check on the headlines of the day. It's great for those who don't have time to spare for full articles and just want the ...
    NewsApp is a Global Media and Information Platform providing Latest News.

---
+++ src/FunctionalComponents/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <>
+            <footer className='background text-light text-center p-2 mt-2'>
+                <p>copyright@newsapp.com</p>
+            </footer>
+        </>
+    )
+}
